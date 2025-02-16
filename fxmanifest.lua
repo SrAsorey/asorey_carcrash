c8e0d73c51a6412e94a97adde9a961111a77f187
Sr.Asorey <braismasorey@gmail.com>
2025-02-16 14:07:21 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Sr.Asorey'
+description 'Car crash effect on screen when you crash your car maded by Sr.Asorey'
+version '1.0'
+
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+    'client/client_open.lua'
+}
